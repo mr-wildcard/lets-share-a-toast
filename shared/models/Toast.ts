e834bb1a10b7caf73957ad636618a5d313fad6a0
Julien Viala <julien.viala@ekino.com>
2021-05-13 23:27:03 +0200

    refactor: interface clean

--- shared/models/Toast.ts
+++ shared/models/Toast.ts
@@ -3,20 +3,21 @@ import { DatabaseToast } from "@shared/firebase";
 import { Subject } from "@shared/models/Subject";
 import { User } from "@shared/models/User";
 
-export interface Toast
-  extends Omit<
-    DatabaseToast,
-    | "organizerId"
-    | "scribeId"
-    | "date"
-    | "createdDate"
-    | "modifiedDate"
-    | "selectedSubjectIds"
-  > {
+type TOASTWithoutFirebaseProperties = Omit<
+  DatabaseToast,
+  | "organizerId"
+  | "scribeId"
+  | "date"
+  | "createdDate"
+  | "modifiedDate"
+  | "selectedSubjectIds"
+>;
+
+export interface Toast extends TOASTWithoutFirebaseProperties {
   date: Date;
   createdDate: Date;
   modifiedDate: Date;
   organizer: User;
   scribe: User;
-  selectedSubjects: Subject[];
+  selectedSubjects?: Subject[];
 }
