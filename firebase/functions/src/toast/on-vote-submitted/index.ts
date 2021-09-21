70bd1afc4505913492911a31575bfc5cebe840f5
Julien Viala <julien.viala@ekino.com>
2021-09-21 13:50:27 +0200

    fix: more efficient cloud function for selected subjects

--- firebase/functions/src/toast/on-vote-submitted/index.ts
+++ firebase/functions/src/toast/on-vote-submitted/index.ts
@@ -4,6 +4,15 @@ import * as functions from "firebase-functions";
 import { DatabaseRefPaths, SubjectsVotes } from "@shared/firebase";
 import { getSelectedSubjectIds } from "@shared/utils";
 
+let maxSelectableSubjects = 0;
+
+admin
+  .database()
+  .ref(`${DatabaseRefPaths.CURRENT_TOAST}/maxSelectableSubjects`)
+  .on("value", (value) => {
+    maxSelectableSubjects = value.val();
+  });
+
 export const onVoteSubmitted = functions.database
   .ref(`${DatabaseRefPaths.VOTING_SESSION}/votes`)
   .onUpdate((change) => {
@@ -13,21 +22,14 @@ export const onVoteSubmitted = functions.database
 
     const votes = change.after.val() as SubjectsVotes;
 
+    const selectedSubjectIds = getSelectedSubjectIds(
+      votes,
+      maxSelectableSubjects
+    );
+
     return admin
       .database()
-      .ref(`${DatabaseRefPaths.CURRENT_TOAST}/maxSelectableSubjects`)
-      .get()
-      .then((value) => value.val())
-      .then((maxSelectableSubjects) => {
-        const selectedSubjectIds = getSelectedSubjectIds(
-          votes,
-          maxSelectableSubjects
-        );
-
-        return admin
-          .database()
-          .ref(DatabaseRefPaths.CURRENT_TOAST)
-          .child("selectedSubjectIds")
-          .set(selectedSubjectIds);
-      });
+      .ref(DatabaseRefPaths.CURRENT_TOAST)
+      .child("selectedSubjectIds")
+      .set(selectedSubjectIds);
   });
