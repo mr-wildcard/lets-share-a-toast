7a8d4b8e281a12f820860045a6d73fe9270e9e25
Julien Viala <julien.viala@ekino.com>
2021-10-05 16:35:12 +0200

    refactor: use helper

--- firebase/functions/src/toast/cancel/index.ts
+++ firebase/functions/src/toast/cancel/index.ts
@@ -1,22 +1,17 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-import { DatabaseRefPaths } from "@shared/firebase";
 import { SubjectStatus, ToastStatus } from "@shared/enums";
 import { getTOASTStatusUtils } from "@shared/utils";
 import { Toast } from "@shared/models";
 
+import { getCurrentTOAST } from "../../helpers/getCurrentTOAST";
 import { cancelledAfterVotes } from "./cancelled-after-votes";
 
 export const cancelToast = functions.https.onCall(async () => {
   functions.logger.info("Cancelling TOAST...");
 
-  const currentToastQuery = await admin
-    .database()
-    .ref(DatabaseRefPaths.CURRENT_TOAST)
-    .get();
-
-  const currentToast: Toast = currentToastQuery.val();
+  const currentToast: Toast = await getCurrentTOAST();
 
   const toastStatusUtil = getTOASTStatusUtils(currentToast.status);
 
