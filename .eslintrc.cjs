ef74567cc4b673c3d42007c826eda584da81e351
Julien Viala <julien.viala@shine.fr>
2022-11-19 16:52:32 +0100

    chore: install eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
